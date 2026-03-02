0ecb7929293aa29f0f999318f1a17cab3a6b3e68
Rald Vibz <vibar_jherald@spcc.edu.ph>
2026-03-02 10:36:05 +0800

    Fix start-sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+php artisan storage:link
 php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+
+chown -R www-data:www-data /var/www/storage
+chmod -R 775 /var/www/storage
+chmod -R 775 /var/www/bootstrap/cache
 
 php-fpm -D
 nginx -g 'daemon off;'
