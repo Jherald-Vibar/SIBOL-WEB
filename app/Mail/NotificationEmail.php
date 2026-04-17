490a5f2a2f4c06cd270955ec6da478f4a47ca79c
Rald Vibz <vibar_jherald@spcc.edu.ph>
2026-04-18 00:42:31 +0800

    Adding Mail

--- app/Mail/NotificationEmail.php
+++ app/Mail/NotificationEmail.php
@@ -8,7 +8,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class NotificationMail extends Mailable
+class NotificationEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
