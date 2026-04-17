95733b0bbdd7636613ff1fd4d42ce3917b3a2223
Rald Vibz <vibar_jherald@spcc.edu.ph>
2026-04-18 00:32:53 +0800

    Adding Mail

--- app/Events/NotificationCreated.php
+++ app/Events/NotificationCreated.php
@@ -2,19 +2,26 @@
 
 namespace App\Events;
 
+use App\Mail\NotificationMail;
 use App\Models\Notification;
 use Illuminate\Broadcasting\PrivateChannel; // ← change this import
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
 
 class NotificationCreated implements ShouldBroadcast
 {
     use Dispatchable, SerializesModels;
 
-    public function __construct(public Notification $notification) {}
+    public function __construct(public Notification $notification)
+    {
+        // Send email as soon as the event is instantiated
+        Mail::to($this->notification->user->email)
+            ->queue(new NotificationMail($this->notification->toArray()));
+    }
 
-    public function broadcastOn(): PrivateChannel // ← and this return type
+    public function broadcastOn(): PrivateChannel
     {
         return new PrivateChannel('notifications.' . $this->notification->user_id); // ← and this
     }
