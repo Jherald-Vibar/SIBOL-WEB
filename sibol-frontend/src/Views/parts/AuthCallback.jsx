53f9358144786ef838d617ce0304b78c2919853d
Rald Vibz <vibar_jherald@spcc.edu.ph>
2026-04-17 21:40:24 +0800

    Fixing Claiming Logic

--- sibol-frontend/src/Views/parts/AuthCallback.jsx
+++ sibol-frontend/src/Views/parts/AuthCallback.jsx
@@ -41,11 +41,7 @@ const AuthCallback = () => {
     }
   }, []);
 
-  return (
-    <div style={{ display:'flex', justifyContent:'center', alignItems:'center', height:'100vh' }}>
-        <p>{status}</p>
-    </div>
-  );
+  return;
 };
 
 
